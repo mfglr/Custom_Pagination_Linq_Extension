f6a170cd2e794c87c6ffd13d3ba330f4b75dbd0f
mfglr <mfgglr@outlook.com>
2024-03-03 04:46:12 +0300

    Add project files.

---
+++ ExpressionTree/CustomPagination.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+
+namespace ExpressionTree
+{
+    public static class CustomPagination
+    {
+        public static IQueryable<TEntity> ToPage<TEntity, TProperty>(
+            this IQueryable<TEntity> queryable,
+            Expression<Func<TEntity, TProperty>> propertyForPagination,
+            TProperty lastValue,
+            bool IsDescending = true,
+            int take = 10
+        )
+            where TProperty : IComparable<TProperty>
+        {
+            var method = typeof(IComparable<TProperty>).GetMethod("CompareTo")!;
+            var left = Expression.Call(propertyForPagination.Body, method, Expression.Constant(lastValue));
+            var right = Expression.Constant(0);
+
+            if (IsDescending)
+                return queryable
+                    .Where(
+                        Expression.Lambda<Func<TEntity, bool>>(
+                            Expression.LessThan(left, right), 
+                            propertyForPagination.Parameters
+                        )
+                    )
+                    .OrderByDescending(propertyForPagination)
+                    .Take(take);
+            return queryable
+                .Where(
+                    Expression.Lambda<Func<TEntity, bool>>(
+                        Expression.GreaterThan(left, right),
+                        propertyForPagination.Parameters
+                    )
+                )
+                .OrderBy(propertyForPagination)
+                .Take(take);
+        }
+    }
+}
---
+++ ExpressionTree/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExpressionTree;
+
+List<Person> context = new()
+{
+    new() { Id = 1, Birthday = new DateTime(2016,7,16), Name = "ali" },
+    new() { Id = 3, Birthday = new DateTime(2007,9,16), Name = "ayse" },
+    new() { Id = 5, Birthday = new DateTime(1969,1,16), Name = "furkan" },
+    new() { Id = 2, Birthday = new DateTime(1978,5,16), Name = "ahmet" },
+    new() { Id = 4, Birthday = new DateTime(1987,4,16), Name = "elif" },
+}; 
+
+//***************************************************************************
+var list0 = context
+    .AsQueryable()
+    .ToPage(x => x.Id, 0, false, 5)
+    .ToList();
+
+/*  output
+ *  { Id = 1, Birthday = new DateTime(2016,7,16), Name = "ali" },
+ *  { Id = 2, Birthday = new DateTime(1978,5,16), Name = "ahmet" },
+ *  { Id = 3, Birthday = new DateTime(2007,9,16), Name = "ayse" },
+ *  { Id = 4, Birthday = new DateTime(1987,4,16), Name = "elif" },
+ *  { Id = 5, Birthday = new DateTime(1969,1,16), Name = "furkan" }
+*/
+//***************************************************************************
+
+
+//***************************************************************************
+var list1 =  context
+    .AsQueryable()
+    .ToPage(x => x.Name, "fatih", take : 3)
+    .ToList();
+
+
+/*  output
+ *  { Id = 4, Birthday = new DateTime(1987,4,16), Name = "elif" },
+ *  { Id = 3, Birthday = new DateTime(2007,9,16), Name = "ayse" },
+ *  { Id = 1, Birthday = new DateTime(2016,7,16), Name = "ali" },
+*/
+//***************************************************************************
+
+
+//***************************************************************************
+var list2 = context
+    .AsQueryable()
+    .ToPage(x => x.Birthday, DateTime.Now, true,2)
+    .ToList();
+
+/*  output
+ *  { Id = 1, Birthday = new DateTime(2016,7,16), Name = "ali" },
+ *  { Id = 3, Birthday = new DateTime(2007,9,16), Name = "ayse" },
+*/
+//***************************************************************************
+
+//***************************************************************************
+var list3 = context
+    .AsQueryable()
+    .ToPage(x => x.Birthday, new DateTime(2007,02,19), true,2)
+    .ToList();
+
+/*  output
+ *  { Id = 4, Birthday = new DateTime(1987,4,16), Name = "elif" },
+ *  { Id = 2, Birthday = new DateTime(1978,5,16), Name = "ahmet" },
+*/
+//***************************************************************************
+
+Console.WriteLine();
+
+
+public class Person
+{
+    public int Id { get; set; }
+    public DateTime Birthday { get; set; }
+    public string Name { get; set; }
+}
+
+
